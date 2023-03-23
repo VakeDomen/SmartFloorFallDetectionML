51aeb8d67dfce58f684169d55bdf407fd0fee86d
VakeDomen <vake.domen94@gmail.com>
2023-03-23 10:19:23 +0100

    removed eval added save to models

--- CNN/CNN.py
+++ CNN/CNN.py
@@ -1,7 +1,6 @@
 import numpy as np
 import gc
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import MaxPool2D
@@ -10,51 +9,15 @@ from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import Dense
 
 
-WINDOW_SIZE = 300
+print("Loading data...")
+X_train = np.load("../data/X_train.npy")
+Y_train = np.load("../data/Y_train.npy")
+X_test = np.load("../data/X_test.npy")
+Y_test = np.load("../data/Y_test.npy")
+       
 
-#!gdown --id 1c69vPJQChbqEWIvQI3sJ6osO-tLVXXPj
-#!gdown --id 1HEeF49zrKyxobAJl1JX7UonVs_F2v69S
+print("Building model...")
 
-#load both as pandas df
-positive_set = pd.read_csv('positiveSet.csv')
-negative_set = pd.read_csv('negativeSet.csv')
-
-# Drop columns that are not needed
-# only keep sample ID and sensor info
-positive_set = positive_set.drop(columns=["person_ID", "fall_category", "tick"])
-negative_set = negative_set.drop(columns=["neg_category", "person_ID", "tick"])
-
-def extract_windows(data, col):
-  for val in data[col].unique():
-    sample_data = data.loc[data[col] == val]
-    sample_data = sample_data.drop(columns=[col]).to_numpy()
-    for window_offset in range(len(sample_data) - WINDOW_SIZE + 1):
-      window = sample_data[ window_offset : window_offset + WINDOW_SIZE, : ]
-      yield window / 65537
-
-
-pdata = list(extract_windows(positive_set, "fall_ID"))
-ndata = list(extract_windows(negative_set, "neg_ID"))
-        
-indices = np.random.choice(len(pdata), size=(len(pdata) - len(ndata)), replace=False)
-pdata = np.delete(pdata, indices, axis=0)
-
-
-X_train, X_test, Y_train, Y_test = train_test_split(
-  np.concatenate((
-    pdata,
-    ndata
-  )),
-  np.concatenate((
-    np.ones(len(pdata)),
-    np.zeros(len(ndata))
-  )),
-  test_size=0.2,
-  random_state=4,
-)
-del pdata, ndata
-        
-        
 model=Sequential()
 #adding convolution layer
 model.add(Conv2D(32,(3,3),activation='relu',input_shape=(300,16,1)))
@@ -68,13 +31,11 @@ model.add(Dense(1,activation='sigmoid'))
 #compiling the model
 model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
 
-
+print("Fitting model...")
 history = model.fit(X_train,Y_train,epochs=10, validation_data=(X_test, Y_test))
-test_loss, test_acc = model.evaluate(X_test,  Y_test, verbose=2)
-model.predict(np.array([ndata[20500]]))
-print(test_loss, test_acc)
 
 
+model.save('../model/CNN')
 
 
 
