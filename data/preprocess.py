9e24acdcd48d690276410e968f17824ccd6c3ceb
VakeDomen <vake.domen94@gmail.com>
2023-03-27 11:44:17 +0200

    added data folding to preproccessing

--- data/preprocess.py
+++ data/preprocess.py
@@ -1,5 +1,7 @@
 import math
+import random
 import numpy as np
+from tqdm import tqdm
 import gc
 import pandas as pd
 import catboost as cb
@@ -8,7 +10,21 @@ from sklearn.utils import shuffle
 
 
 WINDOW_SIZE = 300
+FOLDS = 5
+np.random.seed(42)
 
+
+def split_list(numbers, m):
+    # Shuffle the list randomly
+    random.shuffle(numbers)
+    # Create m empty sublists
+    sublists = [[] for _ in range(m)]
+    # Iterate through the shuffled list and append each number to the smallest sublist
+    for num in numbers:
+        smallest_list = min(sublists, key=len)
+        if num not in smallest_list:
+            smallest_list.append(num)
+    return sublists
 
 def diff(list1, list2):
     li_dif = [i for i in list1 + list2 if i not in list1 or i not in list2]
@@ -38,90 +54,53 @@ negative_set = pd.read_csv('negativeSet.csv')
 positive_set = positive_set.drop(columns=["person_ID", "fall_category", "tick"])
 negative_set = negative_set.drop(columns=["neg_category", "person_ID", "tick"])
 
-print("Selecting falls for train and test...")
-np.random.seed(0)
-train_size = math.floor(len(positive_set["fall_ID"].unique()) * 0.8)
-train_falls_ids = np.random.choice(positive_set["fall_ID"].unique(), train_size, replace=False)
-test_falls_ids = np.setdiff1d(positive_set["fall_ID"].unique(), train_falls_ids) 
-
-
-
-print(f"ALL FALLS: {len(positive_set['fall_ID'].unique())}")
-print(f"TRAIN FALLS: {len(train_falls_ids)}")
-print(f"TEST FALLS: {len(test_falls_ids)}")
+print("Folding positive data...")
+positive_folds = split_list(positive_set["fall_ID"].unique(), FOLDS)
 
 print("Extracting data from the sets...")
-pdata_train = list(extract_windows(positive_set, "fall_ID", train_falls_ids))
-pdata_test = list(extract_windows(positive_set, "fall_ID", test_falls_ids))
-
 ndata = list(extract_windows(negative_set, "neg_ID"))
-
+negative_folds = split_list([*range(len(ndata))], FOLDS)
 
 
 print("Balancing sets...")
+for i in tqdm(range(FOLDS)):
+    X_p = list(extract_windows(positive_set, "fall_ID", positive_folds[i]))
+    X_n = [ndata[j] for j in negative_folds[i]]
 
-# balance the poisitve and negative data sizes
-# train
-X_train_p = pdata_train 
-X_train_n = ndata
-X_test_p = pdata_test
-X_test_n = ndata
+    len_diff = len(X_p) - len(X_n)
+    if len_diff > 0:
+        indices = np.random.choice(len(X_p), size=(len_diff), replace=False)
+        X_p = np.delete(X_p, indices, axis=0)
+    else:
+        indices = np.random.choice(len(X_n), size=(-len_diff), replace=False)
+        X_n = np.delete(X_n, indices, axis=0)
 
-len_diff = len(pdata_train) - len(ndata)
-if len_diff > 0:
-    indices = np.random.choice(len(pdata_train), size=(len_diff), replace=False)
-    X_train_p = np.delete(pdata_train, indices, axis=0)
-else:
-    indices = np.random.choice(len(ndata), size=(-len_diff), replace=False)
-    X_train_n = np.delete(ndata, indices, axis=0)
-
-# test
-len_diff = len(pdata_test) - len(ndata)
-if len_diff > 0:
-    indices = np.random.choice(len(pdata_test), size=(len_diff), replace=False)
-    X_test_p = np.delete(pdata_test, indices, axis=0)
-else:
-    indices = np.random.choice(len(ndata), size=(-len_diff), replace=False)
-    X_test_n = np.delete(ndata, indices, axis=0)
+    positive_folds[i] = X_p
+    negative_folds[i] = X_n
 
 
+X = []
+Y = []
 
+print("Merging data sets...")
+for i in tqdm(range(FOLDS)):
+    X.append(np.concatenate((
+        positive_folds[i],
+        negative_folds[i]
+    )))
+    Y.append(np.concatenate((
+        np.ones(len(positive_folds[i])),
+        np.zeros(len(negative_folds[i]))
+    )))
 
-
-print("Merging sets...")
-
-# concatinate positive and negative data into one set
-X_train = np.concatenate((
-    X_train_p,
-    X_train_n
-))
-
-Y_train = np.concatenate((
-    np.ones(len(X_train_p)),
-    np.zeros(len(X_train_n))
-))
-
-
-X_test = np.concatenate((
-    X_test_p,
-    X_test_n
-))
-
-Y_test = np.concatenate((
-    np.ones(len(X_test_p)),
-    np.zeros(len(X_test_n))
-))
 
 print("Shuffling data...")
-
-X_train, Y_train = shuffle(X_train, Y_train, random_state=0)
-X_test, Y_test = shuffle(X_test, Y_test, random_state=0)
+for i in tqdm(range(FOLDS)):
+    X[i], Y[i] = shuffle(X[i], Y[i], random_state=42)
 
 print("Saving data...")
-
-np.save("X_train.npy", X_train)
-np.save("Y_train.npy", Y_train)
-np.save("X_test.npy", X_test)
-np.save("Y_test.npy", Y_test)
-
+for i in tqdm(range(FOLDS)):
+    np.save(f"folds/X{i}.npy", X[i])
+    np.save(f"folds/Y{i}.npy", Y[i])
+    
 
