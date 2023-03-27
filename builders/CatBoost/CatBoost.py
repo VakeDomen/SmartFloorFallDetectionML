ce6bb931e0998576484df4ac76707a7019efe038
VakeDomen <vake.domen94@gmail.com>
2023-03-27 13:52:16 +0200

    added folding to CatBoost

--- builders/CatBoost/CatBoost.py
+++ builders/CatBoost/CatBoost.py
@@ -1,60 +1,63 @@
 import math
 import numpy as np
+from tqdm import tqdm
 import gc
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import catboost as cb
 
+USE_FOLDS = True
+FOLDS = 5
+
+def build_model():
+    return cb.CatBoostClassifier(
+        iterations=1000,
+        learning_rate=0.1,
+        depth=6,
+        l2_leaf_reg=3,
+        loss_function='Logloss',
+        random_seed=42,
+        task_type='GPU',
+        devices='0:1'
+    )
+
+
+X = []
+Y = []
 print("Loading data...")
-X_train = np.load("../data/X_train.npy")
-Y_train = np.load("../data/Y_train.npy")
-X_test = np.load("../data/X_test.npy")
-Y_test = np.load("../data/Y_test.npy")
-
+if USE_FOLDS:
+    for i in tqdm(range(FOLDS)):
+        X.append(np.load(f"../../data/folds/X{i}.npy"))
+        Y.append(np.load(f"../../data/folds/Y{i}.npy"))
+else:
+    X.append(np.load("../../data/X_train.npy"))
+    Y.append(np.load("../../data/Y_train.npy"))
+       
 print("Reshaping data...")
-X_train = X_train.reshape(*X_train.shape[:-2], -1)
-X_test = X_test.reshape(*X_test.shape[:-2], -1)
-
+for i in tqdm(range(len(X))):
+    X[i] = X[i].reshape(*X[i].shape[:-2], -1)
 
 
 print("Building model...")
-        
-# Define the CatBoost model
-model = cb.CatBoostClassifier(
-    iterations=1000,
-    learning_rate=0.1,
-    depth=6,
-    l2_leaf_reg=3,
-    loss_function='Logloss',
-    random_seed=42,
-    task_type='GPU',
-    devices='0:1'
-)
 
-print("Fitting model...")
-# Fit the model on the training data
-model.fit(X_train, Y_train, verbose=True)
+if USE_FOLDS:
+    print("Fitting models...")
+    for i in range(FOLDS):
+        print(f"Building model {i + 1}...")
+        model = build_model()
+        print(f"Preparing learning folds...")
+        X_train = np.concatenate((X[:i] + X[i+1:]))
+        Y_train = np.concatenate((Y[:i] + Y[i+1:]))
+        print(f"Fitting model {i + 1}/{FOLDS}")
+        model.fit(X_train, Y_train, verbose=True)
+        print("Saving model...")
+        model.save_model(f"../../models/CatBoost/f{i}_CatBoost.cbm", format="cbm")
 
-print("Saving model...")
-model.save_model(
-    "../models/CatBoostModel.cmb",
-    format="cbm",
-    export_parameters=None,
-    pool=None
-)
+else:
+    print("Building model...")
+    model = build_model()
+    print("Fitting model...")
+    history = model.fit(X[0], Y[0], verbose=True)
+    print("Saving model...")
+    model.save("../../models/CatBoost.cbm", format="cbm")
 
-print("Done!")
-
-"""
-# Evaluate the model on the testing data
-Y_pred = model.predict(X_test)
-
-accuracy = np.mean(Y_test == Y_pred)
-n = len(Y_test)
-print(f'Testing Accuracy: {accuracy:.4f} on {n} data points')
-
-Y_pred = model.predict(X_test_p)
-accuracy = np.mean(([1]*len(Y_pred)) == Y_pred)
-n = len(Y_test)
-print(f'Positive test Accuracy: {accuracy:.4f} on {n} data points')
-"""
