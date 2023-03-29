df7c3a45313be99caa26c5e33172901ae1ad809e
VakeDomen <vake.domen94@gmail.com>
2023-03-29 11:34:10 +0200

    added folds to tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -8,60 +8,93 @@ from tensorflow import keras
 from catboost import CatBoostClassifier
 from tensorflow.keras.backend import manual_variable_initialization 
 manual_variable_initialization(True)
+from tqdm import tqdm
 
+FOLDS = 1 
 
 ####################### Setting up data ########################
 print("Loading test data...")
-X_test = np.load("../data/X_test.npy")
-Y_test = np.load("../data/Y_test.npy")
+X = []
+Y = []
+X_flat = []
+X_deep = []
+print("Loading data...")
+for i in tqdm(range(FOLDS)):
+    X.append(np.load(f"../data/folds/X{i}.npy"))
+    Y.append(np.load(f"../data/folds/Y{i}.npy"))
 print("\tDone!")
 
 print("Reshaping data...")
-X_test_flat = X_test.reshape(*X_test.shape[:-2], -1)
-X_test_deep = np.expand_dims(X_test, axis=-1)
+for i in tqdm(range(FOLDS)):
+    X_flat.append(X[i].reshape(*X[i].shape[:-2], -1))
+    X_deep.append(np.expand_dims(X[i], axis=-1))
 
 print("\tDone!")
 
 ####################### Loading models ########################
 
-print("Loading HoeffdingTree model...")
-with open('../models/HoeffdingTreeModel.pickle', 'rb') as handle:
-    model_hoef_tree = pickle.load(handle)
+print("Loading HoeffdingTree models...")
+models_hoef = []
+for i in tqdm(range(FOLDS)):
+    with open(f'../models/HoeffdingTree/f{i}_HoeffdingTree.pickle', 'rb') as handle:
+        models_hoef.append(pickle.load(handle))
 print("\tDone!")
 
-print("Loading CNN model...")
-model_cnn = keras.models.load_model('../models/CNN')
+print("Loading CNN models...")
+models_cnn = []
+for i in tqdm(range(FOLDS)):
+    models_cnn.append(keras.models.load_model(f'../models/CNN/f{i}_CNN.h5'))
 print("\tDone!")
 
-print("Loading transformer model...")
-model_transf = keras.models.load_model("../models/transformer.h5")
+print("Loading transformer models...")
+models_transf = []
+for i in tqdm(range(FOLDS)):
+    models_transf(keras.models.load_model(f"../models/Transformer/f{i}_Transformer.h5"))
 print("\tDone!")
 
-print("Loading CatBoost model...")
-model_cb = CatBoostClassifier().load_model("../models/CatBoostModel.cmb")
+print("Loading CatBoost models...")
+models_cb = []
+for i in tqdm(range(FOLDS)):
+    models_cb.append(CatBoostClassifier().load_model("../models/CatBoostModel.cmb"))
 print("\tDone!")
 
 
 ####################### Calculating ROC AUC ########################
 
 print("\nPredicting probabilities for ROC AUC...")
-y_pred_ht = model_hoef_tree.predict_proba(X_test_flat)
+y_pred_ht = []
+for i in tqdm(range(FOLDS)):
+    y_pred_ht.append(model_hoef_tree.predict_proba(X_test_flat))
 print("\tROC AUC Done! \t| HoeffdingTreeModel")
-y_pred_cnn = model_cnn.predict(X_test_deep)
+y_pred_cnn = []
+for i in tqdm(range(FOLDS)):
+    y_pred_cnn.append(model_cnn.predict(X_test_deep))
 print("\tROC AUC Done! \t| CNN")
-y_pred_transf = model_transf.predict(X_test)
+y_pred_transf = []
+for i in tqdm(range(FOLDS)):
+    y_pred_transf.append(model_transf.predict(X_test))
 print("\tROC AUC Done! \t| Transformer")
-y_pred_cb = model_cb.predict_proba(X_test_flat)
+y_pred_cb = []
+for i in tqdm(range(FOLDS)):
+    y_pred_cb.append(model_cb.predict_proba(X_test_flat))
 print("\tROC AUC Done! \t| CatBoost")
 
 print("\tDone!")
 
 print("Calculating AUC ROC...")
-auc_ht = roc_auc_score(Y_test, y_pred_ht[:,1])
+auc_ht = []
+for i in tqdm(range(FOLDS)):
+    auc_ht.append(roc_auc_score(Y_test, y_pred_ht[:,1]))
 print(f"HoeffdingTree: \t{auc_ht}")
-auc_cnn = roc_auc_score(Y_test, y_pred_cnn)
+auc_cnn = []
+for i in tqdm(range(FOLDS)):
+    auc_cnn.append(roc_auc_score(Y_test, y_pred_cnn))
 print(f"CNN: \t\t{auc_cnn}")
-auc_transf = roc_auc_score(Y_test, y_pred_transf)
+auc_transf = []
+for i in tqdm(range(FOLDS)):
+    auc_transf.append(roc_auc_score(Y_test, y_pred_transf))
 print(f"Transformer: \t{auc_transf}")
-auc_cb = roc_auc_score(Y_test, y_pred_cb[:,1])
+auc_cb = []
+for i in tqdm(range(FOLDS)):
+    auc_cb.append(roc_auc_score(Y_test, y_pred_cb[:,1]))
 print(f"CatBoost: \t{auc_cb}")
