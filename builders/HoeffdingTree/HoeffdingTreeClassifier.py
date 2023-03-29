f075d698bc624a037d7b84b38758d96e324e698e
VakeDomen <vake.domen94@gmail.com>
2023-03-29 11:02:29 +0200

    added folding to HoeffdingTree

--- builders/HoeffdingTree/HoeffdingTreeClassifier.py
+++ builders/HoeffdingTree/HoeffdingTreeClassifier.py
@@ -5,29 +5,68 @@ from skmultiflow.data import RegressionGenerator
 from skmultiflow.trees import HoeffdingTreeClassifier
 from tqdm import tqdm
 import pickle
+import gc
+from tqdm import tqdm
 
+USE_FOLDS = True
+FOLDS = 5
+
+
+
+def build_model():
+    return HoeffdingTreeClassifier(leaf_prediction='mc', split_criterion='gini')
+
+
+X = []
+Y = []
 print("Loading data...")
-X_train = np.load("../data/X_train.npy")
-Y_train = np.load("../data/Y_train.npy")
-X_test = np.load("../data/X_test.npy")
-Y_test = np.load("../data/Y_test.npy")
-
+if USE_FOLDS:
+    for i in tqdm(range(FOLDS)):
+        X.append(np.load(f"../../data/folds/X{i}.npy"))
+        Y.append(np.load(f"../../data/folds/Y{i}.npy"))
+else:
+    X.append(np.load("../../data/X_train.npy"))
+    Y.append(np.load("../../data/Y_train.npy"))
+       
 print("Reshaping data...")
-X_train = X_train.reshape(*X_train.shape[:-2], -1)
-X_test = X_test.reshape(*X_test.shape[:-2], -1)
-
-print("Building model...")
-model = HoeffdingTreeClassifier(leaf_prediction='mc', split_criterion='gini')
-y_pred = np.empty(len(X_train))
+for i in tqdm(range(len(X))):
+    X[i] = X[i].reshape(*X[i].shape[:-2], -1)
 
 
-print("Fitting model...")
-for index in tqdm(range(len(X_train))):
-    y_pred[index] = model.predict([X_train[index]])[0]
-    model.partial_fit([X_train[index]], [Y_train[index]])
+if USE_FOLDS:
+    print("Fitting models...")
+    for i in range(FOLDS):
+        print(f"Building model {i+1}...")
+        model = build_model()
+        print(f"Preparing learning folds...")
+        X_train = np.concatenate((X[:i] + X[i+1:]))
+        Y_train = np.concatenate((Y[:i] + Y[i+1:]))
+        y_pred = np.empty(len(X_train))
 
-print("Saving model...")
-with open('../models/HoeffdingTreeModel.pickle', 'wb') as handle:
-    pickle.dump(model, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        print(f"Fitting model {i+1}/{FOLDS}")
+        
+        for index in tqdm(range(len(X_train))):
+            y_pred[index] = model.predict([X_train[index]])[0]
+            model.partial_fit([X_train[index]], [Y_train[index]])
+
+        print("Saving model...")
+        with open(f'../../models/HoeffdingTree/f{i}_HoeffdingTree.pickle', 'wb') as handle:
+            pickle.dump(model, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        gc.collect()
+
+else:
+    print("Building model...")
+    model = build_model()
+    y_pred = np.empty(len(X[0]))
+
+
+    print("Fitting model...")
+    for index in tqdm(range(len(X[0]))):
+        y_pred[index] = model.predict([X[0][index]])[0]
+        model.partial_fit([X[0][index]], [Y[0][index]])
+
+    print("Saving model...")
+    with open('../../models/HoeffdingTree.pickle', 'wb') as handle:
+        pickle.dump(model, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 print("Done!")
