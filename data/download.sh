0617211d4c9040494473fe5a9430049e2c9eb849
VakeDomen <vake.domen94@gmail.com>
2023-03-30 10:06:44 +0200

    updated download script

--- data/download.sh
+++ data/download.sh
@@ -1,3 +1,14 @@
-gdown --id 1c69vPJQChbqEWIvQI3sJ6osO-tLVXXPj
-gdown --id 1HEeF49zrKyxobAJl1JX7UonVs_F2v69S
+config_file="./../config.ini"
+positive_key="positive_data_gdrive_id"
+negative_key="negative_data_gdrive_id"
+
+pos_id=$(awk -F "=" "/^\[data\]/ {found_section=1} /^\s*${positive_key}\s*=/{if(found_section) print \$2}" "${config_file}")
+neg_id=$(awk -F "=" "/^\[data\]/ {found_section=1} /^\s*${negative_key}\s*=/{if(found_section) print \$2}" "${config_file}")
+
+if command -v gdown >/dev/null 2>&1; then
+    gdown --id $pos_id
+    gdown --id $neg_id 
+else
+    echo "gdown is not installed. Please install it and run the script again."
+fi
 
