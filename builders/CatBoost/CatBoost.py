64a14a7ffa5f0db15bfd559bfa1d10d972a45280
VakeDomen <vake.domen94@gmail.com>
2023-03-30 10:16:01 +0200

    modified CatBoost to use config file

--- builders/CatBoost/CatBoost.py
+++ builders/CatBoost/CatBoost.py
@@ -5,9 +5,11 @@ import gc
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import catboost as cb
+import configparser
 
-USE_FOLDS = True
-FOLDS = 5
+config = configparser.ConfigParser()
+config.read('../../config.ini')
+FOLDS = config.get('data-prepocess', 'folds')
 
 def build_model():
     return cb.CatBoostClassifier(
@@ -25,13 +27,9 @@ def build_model():
 X = []
 Y = []
 print("Loading data...")
-if USE_FOLDS:
-    for i in tqdm(range(FOLDS)):
-        X.append(np.load(f"../../data/folds/X{i}.npy"))
-        Y.append(np.load(f"../../data/folds/Y{i}.npy"))
-else:
-    X.append(np.load("../../data/X_train.npy"))
-    Y.append(np.load("../../data/Y_train.npy"))
+for i in tqdm(range(FOLDS)):
+    X.append(np.load(f"../../data/folds/X{i}.npy"))
+    Y.append(np.load(f"../../data/folds/Y{i}.npy"))
        
 print("Reshaping data...")
 for i in tqdm(range(len(X))):
@@ -40,24 +38,16 @@ for i in tqdm(range(len(X))):
 
 print("Building model...")
 
-if USE_FOLDS:
-    print("Fitting models...")
-    for i in range(FOLDS):
-        print(f"Building model {i + 1}...")
-        model = build_model()
-        print(f"Preparing learning folds...")
-        X_train = np.concatenate((X[:i] + X[i+1:]))
-        Y_train = np.concatenate((Y[:i] + Y[i+1:]))
-        print(f"Fitting model {i + 1}/{FOLDS}")
-        model.fit(X_train, Y_train, verbose=True)
-        print("Saving model...")
-        model.save_model(f"../../models/CatBoost/f{i}_CatBoost.cbm", format="cbm")
-        gc.collect()
-else:
-    print("Building model...")
+print("Fitting models...")
+for i in range(FOLDS):
+    print(f"Building model {i + 1}...")
     model = build_model()
-    print("Fitting model...")
-    history = model.fit(X[0], Y[0], verbose=True)
+    print(f"Preparing learning folds...")
+    X_train = np.concatenate((X[:i] + X[i+1:]))
+    Y_train = np.concatenate((Y[:i] + Y[i+1:]))
+    print(f"Fitting model {i + 1}/{FOLDS}")
+    model.fit(X_train, Y_train, verbose=True)
     print("Saving model...")
-    model.save("../../models/CatBoost.cbm", format="cbm")
-
+    model.save_model(f"../../models/CatBoost/f{i}_CatBoost.cbm", format="cbm")
+    ic.collect()
+print("Done!")
