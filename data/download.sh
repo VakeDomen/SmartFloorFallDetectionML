132c97cc01047efdd57b472b807ec7ea158a09a3
VakeDomen <vake.domen94@gmail.com>
2023-04-06 09:51:08 +0200

    moved data handling to single dataset file

--- data/download.sh
+++ data/download.sh
@@ -1,13 +1,17 @@
 config_file="./../config.ini"
-positive_key="positive_data_gdrive_id"
-negative_key="negative_data_gdrive_id"
+#positive_key="positive_data_gdrive_id"
+#negative_key="negative_data_gdrive_id"
 
-pos_id=$(awk -F "=" "/^\[data\]/ {found_section=1} /^\s*${positive_key}\s*=/{if(found_section) print \$2}" "${config_file}")
-neg_id=$(awk -F "=" "/^\[data\]/ {found_section=1} /^\s*${negative_key}\s*=/{if(found_section) print \$2}" "${config_file}")
+set_key="data_gdrive_id"
+
+data_id=$(awk -F "=" "/^\[data\]/ {found_section=1} /^\s*${set_key}\s*=/{if(found_section) print \$2}" "${config_file}")
+
+#pos_id=$(awk -F "=" "/^\[data\]/ {found_section=1} /^\s*${positive_key}\s*=/{if(found_section) print \$2}" "${config_file}")
+#neg_id=$(awk -F "=" "/^\[data\]/ {found_section=1} /^\s*${negative_key}\s*=/{if(found_section) print \$2}" "${config_file}")
 
 if command -v gdown >/dev/null 2>&1; then
-    gdown --id $pos_id
-    gdown --id $neg_id 
+    gdown --id $data_id
+    #gdown --id $neg_id 
 else
     echo "gdown is not installed. Please install it and run the script again."
 fi
--- data/preprocess.py
+++ data/preprocess.py
@@ -16,8 +16,10 @@ WINDOW_SIZE         = int(config.get('data-preprocess', 'window_size'))
 SEED                = int(config.get('general', 'random_seed'))
 SENSOR_UPPER_BOUND  = int(config.get('data', 'sensor_upper_bound'))
 SENSOR_LOWER_BOUND  = int(config.get('data', 'sensor_lower_bound'))
-POS_ID_COL          = config.get('data', 'positive_data_id_column'))
-NEG_ID_COL          = config.get('data', 'negative_data_id_column'))
+DATA_FILE_NAME      = config.get('data', 'data_file_name')
+GROUP_COL           = config.get('data', 'group_col')
+PREDICT_COL         = config.get('data', 'predict_col')
+
 
 np.random.seed(SEED)
 
@@ -49,25 +51,44 @@ def extract_windows(data, col, vals=[]):
 
 
 print("Loading raw data...")
+data_set = pd.read_csv(DATA_FILE_NAME)
 
-#load both as pandas df
-positive_set = pd.read_csv('positiveSet.csv')
-negative_set = pd.read_csv('negativeSet.csv')
+print("Splitting data by prediction label...")
+labels = data_set[PREDICT_COL].unique()
+data = [[] for _ in range(len(labels)) ]
+for i, lab in enumerate(labels):
+    data[i] = data_set.loc[data_set[PREDICT_COL] == lab]
 
-# Drop columns that are not needed
-# only keep sample ID and sensor info
-positive_set = positive_set.drop(columns=["person_ID", "fall_category", "tick"])
-negative_set = negative_set.drop(columns=["neg_category", "person_ID", "tick"])
-
-print("Folding positive data...")
-positive_folds = split_list(positive_set[POS_ID_COL].unique(), FOLDS)
+print("Folding data...")
+folds = [[] for i in range(len(labels))]
+for i, lab in enumerate(labels):
+    folds[i] = split_list(data[i][GROUP_COL].unique(), FOLDS)
 
 print("Extracting data from the sets...")
-ndata = list(extract_windows(negative_set, NEG_ID_COL))
-negative_folds = split_list([*range(len(ndata))], FOLDS)
+#ndata = list(extract_windows(negative_set, NEG_ID_COL))
+#negative_folds = split_list([*range(len(ndata))], FOLDS)
 
+windowed_data = [[] for _ in range(len(labels))]
+for i, lab in enumerate(labels):
+    for j in range(FOLDS):
+        windowed_data[i].append(list(extract_windows(data_set, GROUP_COL, folds[i][j])))
+        print(f"extracted {len(windowed_data[i])} windows for label {lab} in fold {j} of shape")
 
+#positive_folds = split_list(positive_set[POS_ID_COL].unique(), FOLDS)
+min_sample = len(windowed_data[0][0])
+for i in range(len(labels)):
+    for j in range(FOLDS):
+        if len(windowed_data[i][j]) < min_sample:
+            min_sample = len(windowed_data[i][j])
+
+print(f"min: {min_sample}")
 print("Balancing sets...")
+for i in range(len(labels)):
+    for j in tqdm(range(FOLDS)):
+        indices = np.random.choice(len(windowed_data[i][j]), size=(len(windowed_data[i][j]) - min_sample), replace=False)
+        windowed_data[i][j] = np.delete(windowed_data[i][j], indices, axis=0)
+print(np.array(windowed_data[0]).shape)
+"""
 for i in tqdm(range(FOLDS)):
     X_p = list(extract_windows(positive_set, POS_ID_COL, positive_folds[i]))
     X_n = [ndata[j] for j in negative_folds[i]]
@@ -82,11 +103,20 @@ for i in tqdm(range(FOLDS)):
 
     positive_folds[i] = X_p
     negative_folds[i] = X_n
-
-
+"""
 X = []
 Y = []
 
+for i in tqdm(range(FOLDS)):
+    X_fold = []
+    Y_fold = []
+    for j, lab in enumerate(labels):
+        X_fold.append(windowed_data[j][i])
+        Y_fold.append([lab] * len(windowed_data[j][i]))
+    X.append(np.concatenate(X_fold))
+    Y.append(np.concatenate(Y_fold))
+
+"""
 print("Merging data sets...")
 for i in tqdm(range(FOLDS)):
     X.append(np.concatenate((
@@ -98,7 +128,8 @@ for i in tqdm(range(FOLDS)):
         np.zeros(len(negative_folds[i]))
     )))
 
-
+"""
+print(np.array(X[0]).shape)
 print("Shuffling data...")
 for i in tqdm(range(FOLDS)):
     X[i], Y[i] = shuffle(X[i], Y[i], random_state=SEED)
