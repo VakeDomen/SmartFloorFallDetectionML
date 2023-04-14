558c0b726f680d6338577dc7237d9cf7a1429089
VakeDomen <vake.domen94@gmail.com>
2023-04-14 09:51:06 +0200

    added file header and licence to the run_all.sh script

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,4 +1,38 @@
 #!/bin/bash
+#
+# run_all.sh
+# ----------
+#
+# The script runs all the sub-scripts to run the whole project from start to 
+# finnish. The project will download the needed dataset from google drive,
+# preprocess it and build the models from the data. After the models are built
+# the script for testing the models will run and the test result will be stored 
+# in the results folder. 
+#
+# Author: Domen Vake
+#
+# MIT License
+# Copyright (c) 2023 Domen Vake
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
 ./clear_data.sh
 ./data_setup.sh
 ./clear_models.sh
