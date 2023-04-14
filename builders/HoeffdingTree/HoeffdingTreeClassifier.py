ef2027961be87171a9f3a63750cd0ab42db4dc5e
VakeDomen <vake.domen94@gmail.com>
2023-04-14 08:58:08 +0200

    added batch size to hoefding tree

--- builders/HoeffdingTree/HoeffdingTreeClassifier.py
+++ builders/HoeffdingTree/HoeffdingTreeClassifier.py
@@ -24,6 +24,7 @@ REMOVE_POOR_ATTS            = bool(config.get('model-hoeffding-tree', 'remove_po
 NO_PREPRUNE                 = bool(config.get('model-hoeffding-tree', 'no_preprune'))
 LEAF_PREDICTION             = config.get('model-hoeffding-tree', 'leaf_prediction')
 NB_THRESHOLD                = eval(config.get('model-hoeffding-tree', 'nb_threshold'))
+BATCH_SIZE                  = int(config.get('model-hoeffding-tree', 'batch_size'))
 
 
 def build_model():
@@ -32,11 +33,11 @@ def build_model():
             split_criterion=SPLIT_CRITERION,
             max_byte_size=MAX_BYTE_SIZE,
             memory_estimate_period=MEMORY_ESTIMATE_PERIOD,
-            grace_period=MEMORY_ESTIMATE_PERIOD,
+            grace_period=GRACE_PERIOD,
             split_confidence=SPLIT_CONFIDENCE,
             tie_threshold=TIE_THRESHOLD,
             stop_mem_management=STOP_MEM_MANAGEMENT,
-            remove_poor_atts=REMOVE_POOR_ATTS,
+            #remove_poor_atts=REMOVE_POOR_ATTS,
             no_preprune=NO_PREPRUNE,
             nb_threshold=NB_THRESHOLD            
     )
@@ -62,13 +63,36 @@ for i in range(FOLDS):
     X_train = np.concatenate((X[:i] + X[i+1:]))
     Y_train = np.concatenate((Y[:i] + Y[i+1:]))
     y_pred = np.empty(len(X_train))
-
+    
     print(f"Fitting model {i+1}/{FOLDS}")
-        
+    """        
     for index in tqdm(range(len(X_train))):
         y_pred[index] = model.predict([X_train[index]])[0]
         model.partial_fit([X_train[index]], [Y_train[index]])
+    """
+    num_batches = len(X_train) // BATCH_SIZE
+    for batch_idx in tqdm(range(num_batches)):
+        batch_start = batch_idx * BATCH_SIZE
+        batch_end = (batch_idx + 1) * BATCH_SIZE
+        X_batch = X_train[batch_start:batch_end]
+        Y_batch = Y_train[batch_start:batch_end]
 
+        for index in range(len(X_batch)):
+            y_pred[batch_start + index] = model.predict([X_batch[index]])[0]
+            model.partial_fit([X_batch[index]], [Y_batch[index]])
+
+    # Process remaining samples if the number of samples is not divisible by the batch size
+    if len(X_train) % BATCH_SIZE != 0:
+        remaining_start = num_batches * BATCH_SIZE
+        X_remaining = X_train[remaining_start:]
+        Y_remaining = Y_train[remaining_start:]
+
+        for index in range(len(X_remaining)):
+            y_pred[remaining_start + index] = model.predict([X_remaining[index]])[0]
+            model.partial_fit([X_remaining[index]], [Y_remaining[index]])
+
+            
+            
     print("Saving model...")
     with open(f'../../models/HoeffdingTree/f{i}_HoeffdingTree.pickle', 'wb') as handle:
         pickle.dump(model, handle, protocol=pickle.HIGHEST_PROTOCOL)
